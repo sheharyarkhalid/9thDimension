a8e96687cd26f1e684ce946dcee20ef1daa56062
sheharyar <sheharyarkhalid0313@gmail.com>
2021-09-22 17:49:15 +0500

    initial Design

---
+++ script/custom.js
@@ -0,0 +1,37 @@
+
+$('.open_close_nav').click(function () {
+    $('.responsive_nav_bar').toggleClass('responsive_bar');
+});
+
+
+
+$('.responsive_service_box').click(function () {
+    $('.service_hover_box').toggleClass('responsive_service_toggle');
+});
+
+$('.responsive_industries_box').click(function () {
+    $('.industries_box').toggleClass('responsive_industry_toggle');
+});
+$('.Content_Hub').click(function () {
+    $('.content_hub_mobile').toggleClass('responsive_industry_toggle');
+});
+
+$('.Company_Hub').click(function () {
+    $('.company_hub_mobile').toggleClass('responsive_industry_toggle');
+});
+
+$('.Q_one').click(function () {
+    $('.A_one').toggleClass('answer_box_toggle');
+});
+
+$('.Q_two').click(function () {
+    $('.A_two').toggleClass('answer_box_toggle');
+});
+
+$('.Q_three').click(function () {
+    $('.A_three').toggleClass('answer_box_toggle');
+});
+
+$('.Q_four').click(function () {
+    $('.A_four').toggleClass('answer_box_toggle');
+});
